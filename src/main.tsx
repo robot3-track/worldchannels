516070081579bef7af5797baf0872b72cfdb0748
Yohan Chang <yohanyinyuchang@gmail.com>
2026-06-25 01:34:42 +0000

    fix main.tsx issues and add global.d.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import {StrictMode} from 'react';
+// @ts-ignore: Missing type declarations for react-dom/client
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
