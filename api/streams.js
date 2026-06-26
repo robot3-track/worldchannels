636c2cc28accdc3522130cac2aefefb55fe1c8c3
Yohan Chang <yohanyinyuchang@gmail.com>
2026-06-26 17:29:53 +0000

    fix pushes and new streams

--- api/streams.js
+++ api/streams.js
@@ -1001,6 +1001,7 @@ export default async function handler(request, response) {
       { url: "https://iptv-org.github.io/iptv/countries/be.m3u", category: "country", country: "BE" },
       { url: "https://iptv-org.github.io/iptv/countries/ch.m3u", category: "country", country: "CH" },
       { url: "https://iptv-org.github.io/iptv/countries/kp.m3u", category: "country", country: "KP" },
+      { url: "https://ncdn.telewebion.ir/faratar/live/playlist.m3u8", category: "world cup", country: "IR" },
       { url: "https://iptv-org.github.io/iptv/index.m3u", category: "general"}
     ];
 
