19176d3f82a27ab282e1ece9469d507003b61d65
Yohan Chang <yohanyinyuchang@gmail.com>
2026-06-26 03:08:13 +0000

    fix new streams

--- api/streams.js
+++ api/streams.js
@@ -321,7 +321,7 @@ const staticStreams = [
   { 
     id: "sports-foxsports-1", 
     name: "FS1 (Fox Sports)", 
-    url: "https://cdn.stmify.com/embed-free/v1/fox-spores-mx-jw;allowfullscreen=true;autoplay=true;controls=false;mute=false;loop=false;playsinline=true", 
+    url: "https://freeshot.live/embed/FOXSPORTS1USA.php", 
     isEmbed: true, // Custom flag to help your frontend player switch modes
     category: "world cup", 
     country: "US", 
